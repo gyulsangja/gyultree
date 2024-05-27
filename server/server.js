16e5dca607879ba15aa30d8eb2928c4a05ce950f
gyulsangja <gojekal@naver.com>
2024-05-27 22:04:16 +0900

    v1.3

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.listen(PORT, ()=>{
 
 app.use(express.static(path.join(__dirname, '/dist')))
 
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/dist', 'index.html'))
 })
 
